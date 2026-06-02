a4da4dfcb66b2e9cf93d996e01d121a215a7aada
Akhil <akhil.kuchibhotla2006@gmail.com>
2026-06-02 23:52:17 +0530

    Time: 18 ms (67.39%), Space: 19.6 MB (78.45%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,37 +1,31 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        rows , columns = len(grid) , len(grid[0])
-        num_islands = 0
-        visited = set()
+        max_area_of_island = 0
 
-        if not grid:
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 1:
+                    max_area_of_island = max(max_area_of_island , self.dfs(grid , r , c))
+        return  max_area_of_island
+
+    def dfs(self , grid , r , c):
+
+        if (r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] != 1):
             return 0
 
-        def bfs(r,c):
-           q = collections.deque()
-           visited.add((r,c))
-           q.append((r,c))
-           area = 1
+        grid[r][c] = 0
 
-           while q:
-               row , col = q.popleft()
-               directions = [[-1,0],[1,0],[0,1],[0,-1]]
+        area = 1
 
-               for dr , dc in directions:
-                    r , c = (row + dr) , (col + dc)
-                    if (r in range(rows) and c in range(columns) and grid[r][c] == 1 and (r,c) not in visited):
-                       visited.add((r,c))
-                       q.append((r,c))
-                       area += 1
-           return area
+        area += self.dfs(grid , r + 1, c)
+        area += self.dfs(grid , r - 1, c)
+        area += self.dfs(grid , r , c + 1)
+        area += self.dfs(grid , r , c - 1)
+
+        return area
 
 
 
 
-        max_area = 0
-        for r in range(rows):
-           for c in range(columns):
-               if grid[r][c] == 1 and (r , c) not in visited:
                     
-                    max_area = max(max_area , bfs(r , c))
-        return max_area
+        
