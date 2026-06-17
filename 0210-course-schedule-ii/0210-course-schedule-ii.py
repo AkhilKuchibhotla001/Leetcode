af76f2ffbb96cb9ef968a38313b666f0c039800e
Akhil <akhil.kuchibhotla2006@gmail.com>
2026-06-18 02:09:40 +0530

    Time: 3 ms (75.01%), Space: 21.6 MB (7.1%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,27 +1,25 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-
         hashmap = { i : [] for i in range(numCourses)}
 
         for crs , pre in prerequisites:
             hashmap[crs].append(pre)
 
-        output = []
+        visited , visiting = set() , set()
 
-        visited = set()
-        visiting = set()
+        output = []
 
         def dfs(crs):
             if crs in visiting:
                 return False
             if crs in visited:
                 return True
+
             visiting.add(crs)
 
             for pre in hashmap[crs]:
                 if not dfs(pre):
                     return False
-
             visiting.remove(crs)
             visited.add(crs)
             output.append(crs)
@@ -31,5 +29,4 @@ class Solution:
             if not dfs(crs):
                 return []
         return output
-
         
