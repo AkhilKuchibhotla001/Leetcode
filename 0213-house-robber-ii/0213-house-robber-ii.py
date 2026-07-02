b1ef9f0e71ab4f6edab95a428456a4a1fad1f55a
Akhil <akhil.kuchibhotla2006@gmail.com>
2026-07-02 14:03:51 +0530

    Time: 0 ms (100%), Space: 19.1 MB (93.01%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -2,16 +2,15 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return nums[0]
+        return max(self.max_profit(nums[ 1 : ]) , self.max_profit(nums[: - 1]))
 
-        def helper_function(nums):
-            prev_2 , prev_1 = 0 , 0
-            for num in nums:
-                curr = max(prev_1 , prev_2 + num)
-                prev_2 = prev_1
-                prev_1 = curr
-            return prev_1
-        return max(helper_function(nums[1 :]) , helper_function(nums[ :  -1]))
-
-    
+    def max_profit(self , nums):
+        
+        prev_1 = prev_2 = 0
+        for i in range(len(nums)):
+            curr = max(prev_1 , prev_2 + nums[i])
+            prev_2 = prev_1
+            prev_1 = curr
+        return prev_1
 
         
