ff92417c6ca31c0fa85ad5955da1840d9a0af969
Akhil <akhil.kuchibhotla2006@gmail.com>
2026-07-03 22:27:12 +0530

    Time: 5 ms (55.37%), Space: 21.2 MB (25.93%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -5,23 +5,25 @@ class Solution:
         for crs , pre in prerequisites:
             hashmap[crs].append(pre)
 
-        visited = set()
+        visited = set()   # this is the nodes in the dfs recursion stack
 
         def dfs(crs):
-            if hashmap[crs] == []:
-                return True
             if crs in visited:
                 return False
+            if hashmap[crs] == []:
+                return True
             visited.add(crs)
+
             for pre in hashmap[crs]:
                 if not dfs(pre):
                     return False
             hashmap[crs] = []
-            visited.remove(crs)
+            visited.remove(crs)  # if we didint do this , then it say cycle detected if there is no cycle also
             return True
 
         for crs in range(numCourses):
             if not dfs(crs):
                 return False
         return True
+
         
