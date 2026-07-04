b7212fbbfe8799a481e9a03bdd42368b7a002929
Akhil <akhil.kuchibhotla2006@gmail.com>
2026-07-04 10:06:31 +0530

    Time: 597 ms (68.14%), Space: 28.9 MB (22.05%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,16 +1,17 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
         return self.merge_sort(nums)
+    
+    def merge_sort(self,nums):
+        if len(nums) <= 1:
+            return nums
+        mid = len(nums) // 2
+        left = self.merge_sort(nums[: mid])
+        right = self.merge_sort(nums[mid :])
 
-    def merge_sort(self,arr):
-        if len(arr) <= 1:
-            return arr
-        mid = len(arr) // 2
-        left = self.merge_sort(arr[: mid])
-        right = self.merge_sort(arr[mid :])
+        ans =[]
+
         i = j = 0
-        ans = []
-
         while i < len(left) and j < len(right):
             if left[i] <= right[j]:
                 ans.append(left[i])
@@ -18,9 +19,8 @@ class Solution:
             else:
                 ans.append(right[j])
                 j += 1
-        ans.extend(left[i :])
+        ans.extend(left[i : ])
         ans.extend(right[j :])
-
         return ans
 
         
