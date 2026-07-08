82d35e9555d185b868f26d9b4c4e025b7fb60fc8
Akhil <akhil.kuchibhotla2006@gmail.com>
2026-07-08 22:40:54 +0530

    Time: 0 ms (100%), Space: 19.3 MB (74.05%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,10 +1,12 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[1] * (n) for _ in range(m)]    # ( because the last row and last column takes only 1 path so every value in lat row and last column is 1's)
+        
+        dp = [1] * (n)
 
         for i in range(m - 2 , -1, -1):
-            for j in range(n - 2, -1, -1):
-                dp[i][j] = dp[i][j + 1] + dp[i + 1][j]
-        return dp[0][0]
-        
+            curr = [1] * (n)
+            for j in range(n - 2, -1 , -1):
+                curr[j] = curr[j + 1] + dp[j]
+            dp = curr
+        return dp[0]
         
