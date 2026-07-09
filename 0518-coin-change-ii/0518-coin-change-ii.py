e2413036cdb349e95229adf1458ba90f30de3fb1
Akhil <akhil.kuchibhotla2006@gmail.com>
2026-07-09 19:02:56 +0530

    Time: 183 ms (99.65%), Space: 19.6 MB (63.75%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,10 +1,13 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         dp = [0] * (amount + 1)
-        dp[0] = 1  # no of ways to make amount 0 is 1 (by taking no coins)
+        dp[0] = 1
 
         for coin in coins:
             for i in range(coin , amount + 1):
-                dp[i] += dp[i - coin]
+                dp[i] = dp[i] + dp[i - coin]
         return dp[amount]
 
+
+
+        
