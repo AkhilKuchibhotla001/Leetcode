464e8350c8d0aec8ed7e9d08fb4287d07a0276ec
Akhil <akhil.kuchibhotla2006@gmail.com>
2026-07-14 18:38:38 +0530

    Time: 50 ms (51.2%), Space: 19.1 MB (88.9%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -4,6 +4,5 @@ class Solution:
             return n
         a , b = 0 , 1
         for _ in range(2 , n + 1):
-            a , b = b , a + b
+            a , b = b ,  a + b
         return b
-        
