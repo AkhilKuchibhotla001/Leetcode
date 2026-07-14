0b8019c422083a9937bab9557881c58f1750ba7b
Akhil <akhil.kuchibhotla2006@gmail.com>
2026-07-14 18:44:37 +0530

    Time: 0 ms (100%), Space: 19.3 MB (16.5%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -4,5 +4,7 @@ class Solution:
             return n
         a , b = 1 , 2
         for _ in range(3 , n + 1):
-            a , b = b , a + b
+            a  , b = b , a + b
         return b
+
+        
