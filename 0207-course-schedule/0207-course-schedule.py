2524d314a3c397f9886e1fe83c8cd1330f042cb4
Akhil <akhil.kuchibhotla2006@gmail.com>
2026-07-16 23:06:53 +0530

    Time: 7 ms (44.82%), Space: 21.2 MB (33.26%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,9 +1,9 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         hashmap = {i : [] for i in range(numCourses)}
+
         for crs , pre in prerequisites:
             hashmap[crs].append(pre)
-        
         visited = set()
 
         def dfs(crs):
@@ -11,6 +11,7 @@ class Solution:
                 return False
             if hashmap[crs] == []:
                 return True
+
             visited.add(crs)
 
             for pre in hashmap[crs]:
@@ -19,9 +20,12 @@ class Solution:
             hashmap[crs] = []
             visited.remove(crs)
             return True
-
         for crs in range(numCourses):
             if not dfs(crs):
                 return False
         return True
+
+
+
+
         
