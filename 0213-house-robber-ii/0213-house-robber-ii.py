847e8e6ff231f2563406293767b6540b473e5984
Akhil <akhil.kuchibhotla2006@gmail.com>
2026-07-21 22:33:57 +0530

    Time: 0 ms (100%), Space: 19.5 MB (10.8%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -3,15 +3,13 @@ class Solution:
         if len(nums) == 1:
             return nums[0]
 
-        return max(self.helper_function(nums[1 :]) , self.helper_function(nums[: -1]))
+        return max(self.helper_function(nums[1 : ]) , self.helper_function(nums[ : -1]))
 
-    
+    def helper_function(self , nums):
+        prev_1 , prev_2 = 0 , 0 
 
-    def helper_function(self, nums):
-        n = len(nums)
-        prev_1 , prev_2 = 0 , 0
-        for i in range(n):
-            curr = max(prev_1  , prev_2 + nums[i])
-            prev_1 , prev_2 = curr , prev_1
+        for i in range(len(nums)):
+            curr = max(prev_1 , prev_2 + nums[i])
+            prev_2 , prev_1 = prev_1 , curr
         return prev_1
-
+        
