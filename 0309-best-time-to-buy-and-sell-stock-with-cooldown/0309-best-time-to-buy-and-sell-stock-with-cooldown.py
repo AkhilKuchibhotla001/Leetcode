e06c6412c4604cc15a8667005934f71a65e638fb
Akhil <akhil.kuchibhotla2006@gmail.com>
2026-07-27 21:42:39 +0530

    Time: 1 ms (66.1%), Space: 19.4 MB (82.16%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -4,17 +4,19 @@ class Solution:
         sell = 0
         cool = 0
 
+
         for i in range(1 , len(prices)):
+
             new_buy = max(buy , cool - prices[i])
 
             new_sell = buy + prices[i]
 
-            new_cool = max(cool , sell)
+            new_cool = max(sell , cool)
+
 
             buy = new_buy
-
             sell = new_sell
-
             cool = new_cool
+
         return max(sell , cool)
         
